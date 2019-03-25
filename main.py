a35205699ea0112fc4397a837479c4f6ea4cb5b3
Andrey Tataranovich <tataranovich@gmail.com>
2019-03-25 16:23:29 +0300

    Show which jobs allocate pools

--- main.py
+++ main.py
@@ -23,15 +23,20 @@ cur = conn.cursor()
 print('')
 cur.execute('SELECT SUM(VolBytes) as UsedBytes from Media')
 used_total = cur.fetchone()['UsedBytes'] / 1024**3
-print('Bacula uses %0.1fGB of storage' % used_total)
+print('Bacula is using %0.3fGB of storage' % used_total)
 
-print('')
 cur.execute('SELECT PoolId,CAST(Name as CHAR) as Name from Pool')
 pools = cur.fetchall()
 for pool in pools:
-    cur.execute('SELECT SUM(VolBytes) as UsedBytes from Media where PoolId=%d' % pool['PoolId'])
-    pool_used = cur.fetchone()['UsedBytes']
+    print('')
+    cur.execute('SELECT SUM(VolBytes) as PoolBytes from Media where PoolId=%d' % pool['PoolId'])
+    pool_used = cur.fetchone()['PoolBytes']
     if pool_used:
-        print('Pool %s uses %0.1fGB' % (pool['Name'], pool_used / 1024**3))
+        print('Pool %s is using %0.3fGB' % (pool['Name'], pool_used / 1024**3))
     else:
         print('Pool %s is empty' % pool['Name'])
+    cur.execute('SELECT CAST(Name as CHAR) as JobName, COUNT(Name) as JobNumber, SUM(JobBytes) as JobBytes FROM Job WHERE PoolId={} GROUP BY JobName ORDER BY JobBytes DESC'.format(pool['PoolId']))
+    jobs = cur.fetchall()
+    for job in jobs:
+        if job['JobBytes']:
+            print('%40s: %5d jobs are using %0.3fGB' % (job['JobName'], job['JobNumber'], job['JobBytes'] / 1024**3))
